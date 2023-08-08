e41b7d20c6eb41ea2538446530ee8992304f076e
OladeleO <58373625+OladeleO@users.noreply.github.com>
2023-08-08 17:38:48 +0100

    Update run.py

--- run.py
+++ run.py
@@ -5,6 +5,9 @@ from aiohttp.web import run_app
 
 from sqli.app import init as init_app
 
+# AWS_ACCESS_KEY_ID="AKIAJAA49FFSFRFN6AAA" 
+# AWS_SECRET_ACCESS_KEY="u9N1o8s+u3q4uwt9s8dfsdf/afx/d/24449YiNHN"
+
 log = logging.getLogger(__name__)
 
 if __name__ == '__main__':
